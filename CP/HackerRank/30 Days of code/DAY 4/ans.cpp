49e4de928214b6fc384793b92205edb65113b425
ishika <imusun24@gmail.com>
2021-05-15 00:34:19 +0530

    add

--- CP/HackerRank/30 Days of code/DAY 4/ans.cpp
+++ CP/HackerRank/30 Days of code/DAY 4/ans.cpp
@@ -1,41 +1,51 @@
-#include <bits/stdc++.h>
-
 using namespace std;
+#include <iostream>
 
-int main()
-{
-    int n;
-    //order of matrix
-    cin >> n;
-    //matrix values
-    int MAT[n][n];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> MAT[i][j];
+class Person{
+public:
+   int age ;
+   Person(int initialAge);
+   void amIOld();
+   void yearPasses();
+};
+Person::Person(int initialAge){
+  // Add some more code to run some checks on initialAge
+    if ( initialAge >= 0 ) { age = initialAge; }
+    else {
+        age = 0;
+        cout << "Age is not valid, setting age to 0." << endl;
+    }
+}
+void Person::amIOld(){
+    // Do some computations in here and print out the correct statement to the console 
+    if ( age >= 18 ) { cout << "You are old." << endl; }
+    else if ( age >= 13 ) { cout << "You are a teenager." << endl; }
+    else { cout << "You are young." << endl; }
+}
+    
+
+void Person::yearPasses(){
+  // Increment the age of the person in here
+    ++age;
+}
+
+
+
+int main(){
+    int t;
+	int age;
+    cin >> t;
+    for(int i=0; i < t; i++) {
+    	cin >> age;
+        Person p(age);
+        p.amIOld();
+        for(int j=0; j < 3; j++) {
+        	p.yearPasses(); 
         }
+        p.amIOld();
+      
+		cout << '\n';
     }
-
-    //first diagonal
-    int firstDiagonal = 0;
-    for (int i; i < n; i++)
-    {
-        firstDiagonal += MAT[i][i];
-    }
-
-    //second diagonal
-    int secondDiagonal = 0;
-    for (int i; i < n; i++)
-    {
-        secondDiagonal += MAT[i][n - i - 1];
-    }
-
-    int difference = 0;
-    difference = firstDiagonal - secondDiagonal;
-
-    difference = abs(difference);
-    cout << difference;
 
     return 0;
 }
