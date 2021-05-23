28a2cac175a6dae783354f1f77a1f9d9fc8d4d58
ishika <imusun24@gmail.com>
2021-05-24 01:22:26 +0530

    day 13 lessgo

---
+++ CP/HackerRank/30 Days of code/DAY 13/ans.cpp
@@ -0,0 +1,67 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book
+{
+protected:
+    string title;
+    string author;
+
+public:
+    Book(string t, string a)
+    {
+        title = t;
+        author = a;
+    }
+    virtual void display() = 0;
+};
+
+// Write your MyBook class here
+
+//   Class Constructor
+//
+//   Parameters:
+//   title - The book's title.
+//   author - The book's author.
+//   price - The book's price.
+//
+// Write your constructor here
+
+//   Function Name: display
+//   Print the title, author, and price in the specified format.
+//
+// Write your method here
+class MyBook : public Book
+{
+    int price;
+
+public:
+    MyBook(string title, string author, int p) : Book(title, author)
+    {
+        price = p;
+    }
+    void display()
+    {
+        cout << "Title: " << title << endl
+             << "Author: " << author << endl
+             << "Price: " << price << endl;
+    }
+};
+
+// End class
+
+int main()
+{
+    string title, author;
+    int price;
+    getline(cin, title);
+    getline(cin, author);
+    cin >> price;
+    MyBook novel(title, author, price);
+    novel.display();
+    return 0;
+}
