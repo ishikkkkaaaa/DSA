cee0092bdc8e76b3b126dd49351b3fc53236707f
ishika <imusun24@gmail.com>
2021-06-23 23:18:24 +0530

    add

---
+++ Basics/Basic questions/prime_or_not.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool flag = 0;
+
+    for (int i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Non Prime!!";
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+    {
+        cout << "Prime!!";
+    }
+    return 0;
+}
---
+++ Basics/Patterns/ladder_pyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            if(col<=)
+
+        }
+    }
+}
