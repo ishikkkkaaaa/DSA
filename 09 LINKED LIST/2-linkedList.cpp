260b78473341a96b7e559bb1cf73e4991a7b13c0
ishika kesarwani <imusun24@gmail.com>
2021-09-26 22:21:11 +0530

    add

---
+++ 09 LINKED LIST/2-linkedList.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node* &head,int data)
+{
+    node *n=new node(val);
+
+    if(head==NULL)
+    {
+        head=n;
+    }
+
+    node* temp=head;
+    
+}
+
+int
+main()
+{
+}
